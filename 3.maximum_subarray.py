7698f9c7322137639f7e6b84388edeff2f7ad46c
Soroush <ghaderi.soroush1995@gmail.com>
2020-08-05 11:41:59 +0430

    fix bugs

--- 3.maximum_subarray.py
+++ 3.maximum_subarray.py
@@ -1,12 +1,6 @@
 class Solution(object):
     def maxSubArray(self, nums):
-        maximum_till_there = 0
-        maximum_all = 0
-        for i in range(len(nums)):
-            maximum_till_there += nums[i]
-            
-            if maximum_till_there < 0:
-                maximum_till_there = 0
-            if maximum_all <maximum_till_there:
-                maximum_all = maximum_till_there
-        return maximum_all
+        maximum = [nums[0]]*len(nums)
+        for i in range(1,len(nums)):
+            maximum[i] = max(maximum[i-1], 0)+ nums[i]
+        return max(maximum)
